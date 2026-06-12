1a5914ade27bbbbb4141763ef1dbc0bf38dcbed6
Ashif Ali <84494015+ashifali3147@users.noreply.github.com>
2026-06-12 13:16:04 +0530

    build: register `file_selector` plugin for desktop platforms in example app
    - Add `file_selector_linux` to the Linux plugin registrant and CMake configuration.
    - Add `file_selector_windows` to the Windows plugin registrant and CMake configuration.
    - Add `file_selector_macos` to the macOS plugin registrant.

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import audioplayers_darwin
 import connectivity_plus
 import device_info_plus
 import file_picker
+import file_selector_macos
 import flutter_webrtc
 import livekit_client
 import open_file_mac
@@ -24,6 +25,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   LiveKitPlugin.register(with: registry.registrar(forPlugin: "LiveKitPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
